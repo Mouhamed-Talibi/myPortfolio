a8a5bef17bf2d385d8d908f9b9389d40919c0159
Mouhamed-Talibi <mouha22talibi@gmail.com>
2025-09-08 21:38:17 +0100

    Updating hero section

---
+++ js/typed.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const typed = new Typed(".typed", {
+    strings: [
+        "Developer",
+        "Freelancer",
+        "Backend Developer",
+        "Laravel Developer",
+        "PHP Developer"
+    ],
+    typeSpeed: 80,       // typing speed
+    backSpeed: 50,       // backspacing speed
+    backDelay: 1500,     // delay before backspacing
+    loop: true           // keep looping
+    });
+});
